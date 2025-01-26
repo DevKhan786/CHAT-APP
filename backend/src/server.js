580aef92762d8305e7795c57df11d29d074cb94f
DevVain <InsaneVain786@gmail.com>
2025-01-26 09:44:57 +0000

    Project finished

--- backend/src/server.js
+++ backend/src/server.js
@@ -16,7 +16,7 @@ const __dirname = path.resolve();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-ttng.onrender.com",
     credentials: true,
   })
 );
@@ -27,6 +27,7 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
